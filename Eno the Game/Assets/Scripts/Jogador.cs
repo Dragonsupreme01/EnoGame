ab10c4343dd9a2d80a34bebe8bc9cbbac31cc795
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-16 18:16:48 -0300

    Inicial commit

---
+++ Eno the Game/Assets/Scripts/Jogador.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    private CharacterController _controlador;
+    public float _velocidade = 10f;
+    public float _gravidade = 9.81f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _controlador = GetComponent<CharacterController>();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcularMovimento();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+    }
+
+    private void CalcularMovimento()
+    {
+        float entradaHorizontal = Input.GetAxis("Horizontal");
+        float entradaVertical = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(entradaHorizontal, 0, entradaVertical);
+        Vector3 velocidade = direcao * _velocidade;
+        velocidade.y -= _gravidade;
+
+        velocidade = transform.transform.TransformDirection(velocidade);
+        _controlador.Move(velocidade * Time.deltaTime);
+    }
+}
---
+++ Eno the Game/Assets/Scripts/MouseY.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseY : MonoBehaviour
+{
+
+    // Use this for initialization
+    public float _sensibilidade = 5f;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float _mouseY = Input.GetAxis("Mouse Y");
+
+        Vector3 novaRotacao = transform.localEulerAngles;
+        novaRotacao.x -= _mouseY * _sensibilidade;
+        transform.localEulerAngles = novaRotacao;
+    }
+}
