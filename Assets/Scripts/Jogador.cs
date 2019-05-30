a119837af093e612d48766b331c36e10bb14cdd1
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-30 20:11:22 -0300

    Versão mais recente

---
+++ Assets/Scripts/Jogador.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jogador : MonoBehaviour
+{
+    private CharacterController _controlador;
+    public float _velocidade = 40f;
+    public float _gravidade = 9.81f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _controlador = GetComponent<CharacterController>();
+
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcularMovimento();
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            _velocidade += 30f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            _velocidade -= 30f;
+        }
+    }
+
+    public AudioClip[] arrayDeSoms; // de 5 tipos de passos
+    private void CalcularMovimento() 
+    {
+        /* Se apertou AWSD(MOVIMENTO)
+        Verificar TAG == Chão Se for {
+        Pegar som aleatorio de um array de sons de passo
+        } se não {
+        Pegar som aleatorio de um array de sons de passos
+        }
+       */
+        AudioClip g = arrayDeSoms[UnityEngine.Random.Range(0, arrayDeSoms.Length)];
+        AudioSource.PlayClipAtPoint(g, transform.position);
+        float entradaHorizontal = Input.GetAxis("Horizontal");
+        float entradaVertical = Input.GetAxis("Vertical");
+        Vector3 direcao = new Vector3(entradaHorizontal, 0, entradaVertical);
+        Vector3 velocidade = direcao * _velocidade;
+        velocidade.y -= _gravidade;
+
+        velocidade = transform.transform.TransformDirection(velocidade);
+        _controlador.Move(velocidade * Time.deltaTime);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# EnoGame
+# Eno the Game
+
+### Como usar o GITHUB
+ [Tutorial GITHUB e GIT, o que é e como usar](https://medium.com/@mauriciosantos_94/git-e-github-porque-e-como-usar-18eb94a2dda2)
+ 
+ [Como usar o GIT](https://rogerdudler.github.io/git-guide/index.pt_BR.html)
+
+### Pré-Requisitos
+Versão utilizada da [Unity 2018.1.5f1](https://unity3d.com/pt/get-unity/download/archive).
+
+### Tarefas
+Para adicionar, corrigir, ou ver as tarefas pendentes, verifique os [Issues](https://github.com/AiltonSilvaSC/FMU-Unity-Korsor-TCC/issues) abertos.
+
+### Versões
+Veja as versões para teste nas [tags](https://github.com/AiltonSilvaSC/FMU-Unity-Korsor-TCC/tags) ou releases.
+
+Veja os [marcos](https://github.com/AiltonSilvaSC/FMU-Unity-Korsor-TCC/milestones) de progresso das versões.
+
+Veja todas as partes dos [projetos](https://github.com/AiltonSilvaSC/FMU-Unity-Korsor-TCC/projects) já conclúidos.
+
+ - Para dúvidas consulte o Ailton. :+1:
