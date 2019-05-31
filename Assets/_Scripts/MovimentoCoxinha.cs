affa481db6c9849cf7be73501dd82b856c078214
AiltonSilvaSC <ailtonsscarvalho@gmail.com>
2019-05-30 21:14:27 -0300

    Versão mais recente agr

---
+++ Assets/_Scripts/MovimentoCoxinha.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovimentoCoxinha : MonoBehaviour {
+
+    public GameObject Jogador;
+    private Vector3 _destino;
+    private NavMeshAgent _agent;
+
+    // Use this for initialization
+    void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _agent.destination = Jogador.transform.position;
+        _destino = Jogador.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(_destino, transform.position) > 1.0f)
+        {
+            _destino = Jogador.transform.position;
+            _agent.destination = _destino;
+        }
+    }
+}
